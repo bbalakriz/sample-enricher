e64cf65eb143e8060849b4ebd93cef698ef5d618
Balakrishnan B <bbalasub@redhat.com>
2025-10-01 04:48:44 +0800

    add logging

--- app.py
+++ app.py
@@ -8,14 +8,19 @@ SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
 
 @app.route("/enrich", methods=["POST"])
 def enrich():
+    print("Enriching alert")
     data = request.json
+    print(f"Alert data: {data}")
     data["extra_info"] = "Agent processed this alert!"
+    print("Alert enriched")
     
     msg = {
         "text": f"Enriched Alert:\n*Title:* {data.get('title')}\n*Description:* {data.get('description')}\n*Extra:* {data['extra_info']}"
     }
+    print("Sending to slack")
     requests.post(SLACK_WEBHOOK_URL, json=msg)
 
+    print("Done")
     return jsonify({"status": "ok", "forwarded_to_slack": True})
 
 if __name__ == "__main__":
