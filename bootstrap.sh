1b21a303273d068ba06787f5503b7456350423b6
Bala <balakrishnan.b@gmail.com>
2025-10-25 10:44:59 +0530

    updated repo url

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 ############## obsolete --- deploy sreips-agent ##############
-oc new-app https://github.com/bbalakriz/sreips-agent.git --strategy=source --name=agent -n sreips
+oc new-app https://github.com/bbalakriz/sample-enricher.git --strategy=source --name=agent 
 #  oc delete is/agent bc/agent deploy/agent svc/agent
 oc set env deployment/agent SLACK_WEBHOOK_URL=
